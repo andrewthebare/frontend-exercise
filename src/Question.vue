3a30c38ffa3fac33366b41dc03ec6d803eecb7ad
Andrew Bare <andrewebare21@yahoo.com>
2021-10-03 21:58:42 -0400

    functionally works

---
+++ src/Question.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="question">
+    <h2>{{question.text}}</h2>
+    <div :key="answer" v-for= "answer in question.answers">
+      <input type="radio" :name="question.text" :value="answer">{{answer}}
+    </div>
+    <button @click="onClick()">Advance</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Question',
+  props:{
+    question: Object,
+  },
+
+  methods:{
+    onClick(){
+      // console.log('clicked',  this.question.text)
+
+      let answerSelected = document.querySelector('input[name="'+ this.question.text+ '"]:checked');
+      if(!answerSelected){
+        alert('Please Select an Answer');
+      }else{
+        //passes the answer up the path to Quiz
+        this.$emit('answer-question', answerSelected.value)
+      }
+
+
+
+      // if(document.querySelector('input[name="{{this.question.text}}"]:checked').value){
+      // }
+    }
+  }
+}
+</script>
--- src/Quiz.vue
+++ src/Quiz.vue
@@ -1,8 +1,25 @@
 <template>
-  <pre>{{ JSON.stringify(questions, null, 2) }}</pre>
+  <div>
+    {{ JSON.stringify(questions, null, 2) }}
+    <div :key="question" v-if="this.questions[questionNum]">
+      <Question @answer-question="onAnswer" :question='this.questions[questionNum]'/>
+    </div>
+
+    <div v-if="questionNum >= this.questions.length">
+      <h2>Review</h2>
+      <div :key="i" v-for="i in this.answers">
+        <b>{{i.question}}</b> <i>{{i.answer}}</i>
+      </div>
+    </div>
+
+  </div>
+
+  
 </template>
 
 <script>
+import Question from './Question.vue'
+
 export default {
   props: {
     questions: {
@@ -10,8 +27,28 @@ export default {
       required: true
     }
   },
+  components:{
+    Question,
+  },
+  methods:{
+
+    /**
+     * recieves the answer, stores it, and performs the logic 
+     * to move the quiz along
+     */
+    onAnswer(answer){
+      this.answers[this.questions[this.questionNum].text] = {
+          question: this.questions[this.questionNum].text,
+          answer: answer,
+        };
+      this.questionNum++;
+    }
+  },
   data() {
-    return {};
+    return{
+      answers:{},
+      questionNum: 0,
+    }
   }
 };
 </script>
